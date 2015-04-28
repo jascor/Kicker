967c616e0086004eb900538479964e739c769695
Jason Chen <jason@jcor.me>
2015-04-28 12:29:13 -0700

    F

--- app/services/fastly.rb
+++ app/services/fastly.rb
@@ -4,13 +4,27 @@ class Fastly
   FASTLY_APP_SERVICE = ENV['FASTLY_SERVICE_ID']
 
   def self.purge_key(key)
-    session = Faraday.new(FASTLY_API_BASE_URL) do |s|
+    session = create_fastly_session
+
+    session.post do |p|
+      p.url "/service/#{FASTLY_APP_SERVICE}/purge/#{key}"
+    end
+  end
+
+  def self.purge_all
+    session = create_fastly_session
+
+    session.post do |p|
+      p.url "/service/#{FASTLY_APP_SERVICE}/purge_all"
+    end
+  end
+
+  private
+
+  def self.create_fastly_session
+    Faraday.new(FASTLY_API_BASE_URL) do |s|
       s.request :url_encoded
       s.adapter Faraday.default_adapter
-    end
-
-    purge = session.post do |p|
-      p.url "/service/#{FASTLY_APP_SERVICE}/purge/#{key}"
     end
   end
 end
